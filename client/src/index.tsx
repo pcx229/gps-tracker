12c5c469a84c091efb5669c30f5409f9c9a15e45
pcx229 <pcx229@gmail.com>
2021-06-26 23:19:45 +0300

    added pwa

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import store from './state/store'
 import { Provider } from 'react-redux'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -11,6 +12,11 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
